88ff09b6b6ab3d4022f23d64c9de13763ae56b22
memorycancel <memorycancel@gmail.com>
2023-11-11 10:58:07 +0800

    Update C2 lesson 16

--- C2/16/3710.cpp
+++ C2/16/3710.cpp
@@ -3,6 +3,30 @@ using namespace std;
 
 int main()
 {
-    
+    string s;
+    getline(cin,s);
+    string word_buf="";
+    int is_finded=-1;
+    for(int i=0;i<s.size();i++)
+    {
+        if(s[i]!=' ') word_buf+=s[i];
+        else
+        {
+            if(word_buf!=""&&word_buf=="kjtscholl")
+            {
+                is_finded=1;
+                s[i-2]='o';
+            }
+            word_buf="";
+        }
+    }
+    if(word_buf!=""&&word_buf=="kjtscholl")
+    {
+        is_finded=1;
+        s[s.size()-2]='o';
+    }
+    if(is_finded==-1) cout<<-1;
+    else cout<<s;
+
     return 0;
 }
--- C2/16/4043.cpp
+++ C2/16/4043.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main()
 {
-    
+    int start;
+    char c;
+    cin>>start>>c;
+    cin.ignore(); // this line is very important
+    string s;
+    getline(cin,s);
+    string res="";
+    for(int i=start+1;i<s.size();i++)
+        if(s[i]!=' ') res+=s[i];
+    cout<<res;
+
     return 0;
 }
