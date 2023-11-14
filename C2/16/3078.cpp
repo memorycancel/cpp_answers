3e692b4e8e51b7759d00a0b352f79419bd301ab9
memorycancel <memorycancel@gmail.com>
2023-11-14 18:14:53 +0800

    Update C2 16 3078.cpp

--- C2/16/3078.cpp
+++ C2/16/3078.cpp
@@ -3,6 +3,14 @@
 #include <algorithm>
 using namespace std;
 
+// why?
+// For Example1: 5  50 -> 550>505   => 5  > 50
+// For Example2: 12 13 -> 1312>1213 => 13 > 12
+bool comps(string a,string b)
+{
+    return (a+b)>(b+a);
+}
+
 int main()
 {
     int n; cin>>n;
@@ -13,7 +21,7 @@ int main()
         cin>>s;
         vs.push_back(s);
     }
-    sort(vs.rbegin(),vs.rend());
+    sort(vs.begin(),vs.end(),comps);
     string res_str="";
     for(string s:vs) res_str+=s;
     cout<<res_str;
