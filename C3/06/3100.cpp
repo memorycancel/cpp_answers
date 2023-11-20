46c66692b8626898442898a7e3bc3728eb2cc780
TOMMY <memorycancel@gmail.com>
2023-11-20 11:26:49 +0800

    Update 3100.cpp

--- C3/06/3100.cpp
+++ C3/06/3100.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#define ll long long
 using namespace std;
 
 int main()
 {
-
+    ll n;cin>>n;
+    ll a[n+1][n+1]={};
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            if(j==0||j==i-1)
+            {
+                a[i][j]=1;
+                cout<<1<<' ';
+            }
+            else
+            {
+                a[i][j]=a[i-1][j-1]+a[i-1][j];
+                cout<<a[i][j]<<' ';
+            }
+        }
+        cout<<endl;
+    }
     
     return 0;
 }
